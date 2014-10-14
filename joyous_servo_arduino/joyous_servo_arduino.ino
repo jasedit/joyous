a559a555f88a1880f21b8beb29377712d4c3cd11
newtonj <hakujin1337@gmail.com>
2014-10-14 11:35:30 -0400

    simplify arduino-side code to merely publishing servo angles

--- joyous_servo_arduino/joyous_servo_arduino.ino
+++ joyous_servo_arduino/joyous_servo_arduino.ino
@@ -30,8 +30,6 @@
 ros::NodeHandle  nh;
 
 Servo servo;
-geometry_msgs::TransformStamped t;
-tf::TransformBroadcaster broadcaster;
 char base_link[] = "/servo";
 char odom[] = "/odom";
 
@@ -39,7 +37,6 @@ void servo_cb( const sensor_msgs::Joy& cmd_msg){
   uint16_t value = (cmd_msg.axes[0]*SCALE)+90;
   servo.write(value); //set servo angle, should be from 0-180   
   float angle = servo.read();
-  publish(angle);
   digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
 }
 
@@ -49,23 +46,9 @@ void setup(){
   pinMode(13, OUTPUT);
 
   nh.initNode();
-  broadcaster.init(nh);
   nh.subscribe(sub);
-  
-  servo.attach(9); //attach it to pin 9
-}
 
-void publish(float angle)
-{
-  t.header.frame_id = odom;
-  t.child_frame_id = base_link;
-  t.transform.translation.x = 0.0;
-  t.transform.rotation.x = 1.0/(1+(angle*angle));
-  t.transform.rotation.y = 0.0;
-  t.transform.rotation.z = 0.0;
-  t.transform.rotation.w = angle/(1+(angle*angle));
-  t.header.stamp = nh.now();
-  broadcaster.sendTransform(t);
+  servo.attach(9); //attach it to pin 9
 }
 
 //seems to be required
